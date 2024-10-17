6cf707d6f1fad8230eb22ba222cd7b69a1048878
azurecorner <leyegora@yahoo.fr>
2024-10-17 13:17:57 +0200

    add ssh

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ansible-workshop
+
+ansible-playbook  gen-ssh-key.yml
+
+ls -l /tmp/logornerkeys*
+
+
+ansible-playbook deploy-ssh-key.yml --ask-pass
+
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook deploy-ssh-key.yml --ask-pass
--- iac/terraform/script.sh
+++ iac/terraform/script.sh
@@ -9,7 +9,11 @@ sudo apt install -y git-all
 
 git --version
 
+sudo sudo apt-get install sshpass
+
+
 #sudo git clone https://github.com/azurecorner/ansible-workshop.git
+#cd ansible-workshop/ansible
 
 sudo mkdir -p /etc/ansible
 # sudo cp ~/ansible-workshop/iac/terraform/hosts /etc/ansible/hosts
@@ -32,19 +36,29 @@ EOF
 
 # #create a private and public ssh key pair
 
-# ssh-keygen -t rsa
+sudo ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N "Ansible1#12"
 
 # #grant execute permissions to owner (virtual machine)
-# chmod 755 ~/.ssh 
+ chmod 755 ~/.ssh 
 
 # #set read only permissions to the authorized_keys file
 
-# touch ~/.ssh/authorized_keys
-# chmod 644 ~/.ssh/authorized_keys
+ touch ~/.ssh/authorized_keys
+ chmod 644 ~/.ssh/authorized_keys
 
 # # next we want to ssh to the ubuntu virtual machine without entering our password
 
-# sudo ssh-copy-id logcorner@52.232.102.57
+# sudo ssh-copy-id logcorner@10.3.1.5
+
+# # we can also use sshpass to ssh to the virtual machine without entering our password
+
+# sudo sshpass -p 'Ansible1#12' ssh-copy-id -i ~/.ssh/id_rsa.pub logcorner@"${arg1.ipaddress}"
+# sudo sshpass -p 'Ansible1#12' ssh-copy-id -i ~/.ssh/id_rsa.pub logcorner@"${arg2.ipaddress}"
+# sudo sshpass -p 'Ansible1#12' ssh-copy-id -i ~/.ssh/id_rsa.pub logcorner@"${arg3.ipaddress}"
+
+## sshpass -p 'vm-pass-word' ssh logcorner@10.3.1.5
+## cat ~/.ssh/authorized_keys
+
 # # retrive private key
 # ~/.ssh 
 # cat id_rsa
